3491069bf955c908176d59542c42e420ad1e536d
vigneshrao77 <peggarlavigneshrao1@gmail.com>
2026-08-08 18:49:20 +0530

    hello

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b === 0) {
+    if (b === 5) {
         throw new Error("Division by zero is not allowed.");
     }
     return a / b;
